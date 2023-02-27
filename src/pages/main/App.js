2effad1690fce67982d16483d9a0111868699559
aidysh <aidysh0324@gmail.com>
2023-02-27 23:28:58 +0700

    add hooks navigate

--- src/pages/main/App.js
+++ src/pages/main/App.js
@@ -1,9 +1,24 @@
-import './App.css';
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import "./App.css";
 
 function App() {
-  return(
-    <div>Hello world</div>
-  )
+  const navigate = useNavigate();
+
+  function getCookie(name) {
+    const value = `; ${document.cookie}`;
+    const parts = value.split(`; ${name}=`);
+    if (parts.length === 2) return parts.pop().split(";").shift();
+  }
+  const userToken = getCookie("token");
+
+  useEffect(() => {
+    if (!userToken) {
+      navigate("auth");
+    }
+  });
+
+  return <div>Hello world</div>;
 }
 
 export default App;
