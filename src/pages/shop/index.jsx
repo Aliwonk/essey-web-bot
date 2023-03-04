4ee3a2521e797918d29aaebe625f8727ce3b7b11
aidysh <aidysh0324@gmail.com>
2023-03-04 22:28:35 +0700

    add news in shop page

--- src/pages/shop/index.jsx
+++ src/pages/shop/index.jsx
@@ -34,20 +34,20 @@ export default function Shop() {
                     <div className={styles.infShop}>
                         <div className={styles.genInfShop}>
                             <div className={styles.colName}>
-                                <p className={styles.caption}>Название</p>
+                                <p className={styles.title}>Название</p>
                                 <p>Speed Pizza</p>
                             </div>
                             <div className={styles.colCategory}>
-                                <p className={styles.caption}>Категория</p>
+                                <p className={styles.title}>Категория</p>
                                 <p className={styles.category}>Рестораны</p>
                             </div>
                         </div>
                         <div className={styles.annotShop}>
-                            <p className={styles.caption}>Аннотация</p>
+                            <p className={styles.title}>Аннотация</p>
                             <p className={styles.annotation}>Пиццы со скоростью света</p>
                         </div>
                         <div className={styles.navigation}>
-                            <p className={styles.caption}>Информация</p>
+                            <p className={styles.title}>Информация</p>
                             <div className={styles.navBtns}>
                                 <div className={styles.btnWorkTime}>
                                     <p>Открыт</p>
@@ -70,11 +70,24 @@ export default function Shop() {
                             </div>
                         </div>
                         <div className={styles.additInf}>
-                            <p className={styles.caption}>Доп. информация</p>
+                            <p className={styles.title}>Доп. информация</p>
                             <div className={styles.listInf}>
                                 <p>Бесплатная доставка от 300р</p>
                                 <p>Хорошее качество</p>
                                 <p>Быстрая пицца</p>
+                            </div>
+                        </div>
+
+                        <div className={styles.news}>
+                            <p className={styles.caption}>Новости и акции</p>
+                            <div className={styles.listNews}>
+                                <div className={styles.imgNews}>
+                                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ2KD7-CockFL_lf9h_NvWCuJ6zupJ70kyQdA&usqp=CAU" alt="Картинка" />
+                                </div>
+                                <div className={styles.infNews}>
+                                    <p className={styles.dateNews}>23.03.2023</p>
+                                    <p className={styles.descNews}>Получи пиццу 2 раза дешевле при вызове 5 звонков президенту с вопросом: что для него значит водка и виски</p>
+                                </div>
                             </div>
                         </div>
                     </div>
